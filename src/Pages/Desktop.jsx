4890cbd326317da8a19073f9de1b81f2a1099856
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-13 03:59:26 +0900

    Update Desktop.jsx

--- src/Pages/Desktop.jsx
+++ src/Pages/Desktop.jsx
@@ -1,12 +1,14 @@
 import DesktopApps from "../Components/DesktopApps";
 import TaskManager from "../Components/TaskManager";
 import React, { useState } from 'react';
+import ContextMenu from "../Managers/Context/ContextMenu";
 const Desktop = () => {
 
     return(
         <div className="Page Desktop"> 
             <DesktopApps />
             <TaskManager />
+            <ContextMenu /> 
         </div>
     );
 }
