664c2148710721acf812e69568e6301455ed07bb
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-13 15:40:40 +0900

    Create PowerManager.jsx

---
+++ src/Api/Libs/VioletClientManager/Core/Scripts/PowerManager.jsx
@@ -0,0 +1,11 @@
+import AccentButton from "../../../VioletUiLib/Libs/uiElements/Buttions/AccentButton";
+
+const PowerManager =() => {
+    return(
+        <div>
+            <button>Блокировка</button>
+            <AccentButton title="Выключение"/>
+        </div>
+    );
+}
+export default PowerManager;
