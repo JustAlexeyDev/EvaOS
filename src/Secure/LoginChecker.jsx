86d58df516eea503c079db302e1e72e9ecc7ec74
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-13 12:03:10 +0900

    Update LoginChecker.jsx

--- src/Secure/LoginChecker.jsx
+++ src/Secure/LoginChecker.jsx
@@ -4,7 +4,7 @@ import {useNavigate } from "react-router-dom";
 export default function LoginChecker() {
     const navigate = useNavigate();
     useEffect(() => {
-        if (localStorage.getItem("user") == "true") {
+        if (localStorage.getItem("user") == true) {
             navigate("/Login");
         }
     }, [navigate]);    
