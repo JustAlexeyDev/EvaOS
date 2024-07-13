a23bdf7276a6c46505d1372864d1534c20982629
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-13 12:05:10 +0900

    Create LoginChecker.jsx

---
+++ src/Security/LoginChecker.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import {useNavigate } from "react-router-dom";
+
+export default function LoginChecker() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (localStorage.getItem("user")) {
+            navigate("/Login");
+        }
+    }, [navigate]);    
+}
+
+
