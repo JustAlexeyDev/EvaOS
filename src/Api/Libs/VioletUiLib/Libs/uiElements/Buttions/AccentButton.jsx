d0b0e04ffcdd6712b1fd9fcb7f89e9b1d07a904c
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-13 15:40:44 +0900

    Create AccentButton.jsx

---
+++ src/Api/Libs/VioletUiLib/Libs/uiElements/Buttions/AccentButton.jsx
@@ -0,0 +1,10 @@
+import './Styles/AccentButton.css';
+
+const AccentButton = ({title}) => {
+    return(
+        <button className="Accent--Button--VioletUi">
+            {title}
+        </button>
+    );
+}
+export default AccentButton;
