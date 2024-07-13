c1f16702b496c1230bfc77f1c8eb947abbde609f
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-13 12:03:07 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 const container  = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-import './Assets/Css/Global.css';
-import './Assets/Css/Reset.css';
+import './Env/Styles/Global.css';
+import './Env/Styles/Reset.css';
 import Core from "./Core";
 
 root.render(
