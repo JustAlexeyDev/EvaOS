3e0820ce6a7fe428000bd1f39ba4596f50e5eee5
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-14 13:01:16 +0900

    Update VioletUiLoadingBar.jsx

--- src/Api/Libs/VioletUiLib/Libs/uiElements/ProgressBars/LoadingBar/VioletUiLoadingBar.jsx
+++ src/Api/Libs/VioletUiLib/Libs/uiElements/ProgressBars/LoadingBar/VioletUiLoadingBar.jsx
@@ -1,6 +1,7 @@
 import './VioletUiLoadingBar.css';
 
 const VioletUiLoadingBar = ({progress}) => {
+    
     return(
         <div className="VioletUiLoadingBar--Container">
             <div className="VioletUiLoadingBar--Container--Box">
