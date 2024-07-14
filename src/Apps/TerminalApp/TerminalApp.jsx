d8e7f305af99cbb60f451f8d799fc196b28ab72f
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-14 15:38:13 +0900

    Update TerminalApp.jsx

--- src/Apps/TerminalApp/TerminalApp.jsx
+++ src/Apps/TerminalApp/TerminalApp.jsx
@@ -1,7 +1,7 @@
-import "./VioletUiTest.css";
+import "./TerminalApp.css";
 import React, { useState, useEffect } from 'react';
 import WindowManager from '../../Api/Libs/VioletClientManager/Core/Managers/Windows/WindowManager';
-
+import data from './history.json';
 
 const TerminalApp = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,9 +24,24 @@ const TerminalApp = () => {
 
       {isOpen && (
         <WindowManager title="Terminal" onClose={handleClose}>
-          <div className="">
+          <div className="Terminal--Container">
 
-
+            <div className="Terminal--Container--Messanges">
+                {data.leigth > 0 && (
+                    <div>
+                        {data.map((messages) => (
+                            <div className="Terminal--Container--Messanges">
+                                {messages.command}
+                                dlajksndkas
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+            <div className="Terminal--Container--Inputzone">
+                <input type="text" />
+                <button>Send</button>
+            </div>
 
           </div>
         </WindowManager>
