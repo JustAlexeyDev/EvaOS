d33fb3ad41b0a9c031d8775da9cc1c057a5a0e91
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-14 11:01:54 +0900

    Create SecondButton.jsx

---
+++ src/Api/Libs/VioletUiLib/Libs/uiElements/Buttions/SecondButton.jsx
@@ -0,0 +1,11 @@
+
+
+
+const SecondButton = ({title}) => {
+    return(
+        <button className="Second--button--VioletUi">
+            {title}
+        </button>
+    );
+}
+export default SecondButton;
