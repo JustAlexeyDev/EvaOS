b9d0a78185969ad014485f0065fd23bf92a6051c
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-19 00:06:11 +0900

    Update TerminalApp.jsx
    Added removeUser and logout

--- Interface/src/Apps/TerminalApp/TerminalApp.jsx
+++ Interface/src/Apps/TerminalApp/TerminalApp.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {useNavigate } from "react-router-dom";
 import './TerminalApp.css';
 import data from './history.json';
 import WindowManager from '../../Api/Libs/VioletClientManager/Core/Managers/Windows/WindowManager';
@@ -7,6 +8,8 @@ const TerminalApp = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [history, setHistory] = useState(data);
   const [inputValue, setInputValue] = useState('');
+  const navigate = useNavigate();
+  const userLogged = localStorage.getItem("user");
 
   useEffect(() => {
     localStorage.setItem('terminalHistory', JSON.stringify(history));
@@ -36,15 +39,45 @@ const TerminalApp = () => {
     setHistory([]);
   };
 
+  const removeUser = () => {
+    localStorage.removeItem('fogotQuestion');
+    localStorage.removeItem('user');
+    localStorage.removeItem('password');
+    setIsOpen(false);
+
+        if (userLogged) {
+            console.log("Checked");
+        } if (!userLogged) {
+            navigate("/");
+            console.error("NotAuth");
+        }
+  };
+
+  const logout = () => {
+    navigate("/Login")
+  }
+
   const handleCommand = (command) => {
     switch (command) {
       case 'help':
-        return 'Available commands: help, clear';
+        return 'Available commands: help, clear, logout';
       case 'clear':
         handleClearHistory();
         return 'History cleared';
+      case 'removeUser':
+        removeUser();
+        return 'Removing user...';
+      case 'logout':
+        logout();
+        return 'loggin out..'
       default:
         return `Command not found: ${command}`;
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSend();
     }
   };
 
@@ -58,24 +91,29 @@ const TerminalApp = () => {
 
       {isOpen && (
         <WindowManager title="Terminal" onClose={handleClose}>
-        <div className="Terminal--Container">
-          <div className="Terminal--Messages">
-            {history.length > 0 && (
-              <div>
-                {history.map((message, index) => (
-                  <div key={index} className="Terminal--Message">
-                    <span className='Terminal--Message--User'>{message.command}</span>
-                    <span>{handleCommand(message.command)}</span>
-                  </div>
-                ))}
-              </div>
-            )}
+          <div className="Terminal--Container">
+            <div className="Terminal--Messages">
+              {history.length > 0 && (
+                <div>
+                  {history.map((message, index) => (
+                    <div key={index} className="Terminal--Message">
+                      <span className='Terminal--Message--User'>{message.command}</span>
+                      <span>{handleCommand(message.command)}</span>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+            <div className="Terminal--Inputzone">
+              <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Нажмите Enter для ввода"
+                onKeyPress={handleKeyPress}
+              />
+            </div>
           </div>
-          <div className="Terminal--Inputzone">
-            <input type="text" value={inputValue} onChange={handleInputChange} />
-            <button onClick={handleSend}>Send</button>
-          </div>
-        </div>
         </WindowManager>
       )}
     </>
