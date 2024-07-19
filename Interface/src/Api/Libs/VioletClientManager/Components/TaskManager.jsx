c9661ec4d6bb17dbbcdf50d7203dd53b4d20fefe
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-20 01:00:27 +0900

    Update TaskManager.jsx

--- Interface/src/Api/Libs/VioletClientManager/Components/TaskManager.jsx
+++ Interface/src/Api/Libs/VioletClientManager/Components/TaskManager.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react"
 import AppsMenu from "./AppsMenu";
 import { format } from "date-fns";
+import logo from './Menu.svg';
 const TaskManager =  () => {
     const [openAppsMenu, setOpenAppsMenu] = useState(false);
     const [time, setTime] = useState(new Date().toLocaleTimeString());
@@ -29,7 +30,7 @@ const TaskManager =  () => {
                 <AppsMenu />
             }
             <button className="App--Icon" onClick={handleOpenAppsMenu}>
-                <img src="https://imgs.search.brave.com/B5T_S2RQl4H6vTtPSbe6NPhXxwb_6dI6r06OmNZnRk8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tYXhj/ZG4uaWNvbnM4LmNv/bS9wbGF0Zm9ybXMv/YmVuZWZpdHMvaWNv/bi93aW5kb3dzLnN2/Zw" />
+                <img src={logo} />
             </button>
 
             <div>
