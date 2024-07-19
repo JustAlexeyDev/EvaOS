2f4927c549e1de236f52d8eb609b95b55134b9c9
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-19 23:58:11 +0900

    Update AlertConfirm.jsx

--- Interface/src/Api/Libs/VioletClientManager/Core/Scripts/Security/Alert/Common/AlertConfirm.jsx
+++ Interface/src/Api/Libs/VioletClientManager/Core/Scripts/Security/Alert/Common/AlertConfirm.jsx
@@ -3,7 +3,6 @@
 const AlertConfirm = ({onClick}) => {
     return(
         <div className="AlertConfirm">
-            <a>Отмена</a>
             <button onClick={onClick}>Подтвердить</button>
         </div>
     );
