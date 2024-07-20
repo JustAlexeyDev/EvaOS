9f09b40fc06ddf4ed26db6d3371718a0b343d98b
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-20 18:02:57 +0900

    Update DesktopApps.jsx

--- Interface/src/Components/DesktopApps.jsx
+++ Interface/src/Components/DesktopApps.jsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from "react";
 
-import Notepad from "../Apps/Note/Notepad";
+import Notepad from "../Apps/Note/NotepadApp";
 import TodoApp from "../Apps/TodoApp/ToDoApp";
-import Settings from "../Apps/Settings/Settings";
+import Settings from "../Apps/SettingsApp/SettingsApp";
 import VioletUiTest from "../Apps/VioletUiTest/VioletUiTest";
 import TerminalApp from "../Apps/TerminalApp/TerminalApp";
 
@@ -11,8 +11,8 @@ const DesktopApps = () => {
     return(
         <div className="DesktopApps">
             <Notepad />
-            <TodoApp />
             <Settings />
+            <TodoApp />
             <VioletUiTest />
             <TerminalApp />
         </div>
