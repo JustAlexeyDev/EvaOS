e79335804463d6743b3140e3ef34061cb3017d0f
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-22 01:22:57 +0900

    Create PowerManager.tsx

---
+++ Interface/src/Api/Libs/VioletClientKernel/Core/Scripts/PowerManager.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ViolitUiAccentButton from "../../../VioletUiLib/Libs/uiElements/Buttions/ViolitUiAccentButton";
+
+const PowerManager: React.FC =() => {
+    
+    return(
+        <div>
+            <button>Блокировка</button>
+            <ViolitUiAccentButton title="Выключение"/>
+        </div>
+    );
+}
+export default PowerManager;
