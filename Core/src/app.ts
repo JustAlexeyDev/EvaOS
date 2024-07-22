b33913adca7346a05ff3f7f74abd8a4809f1d335
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-23 02:24:40 +0900

    Update app.ts

--- Core/src/app.ts
+++ Core/src/app.ts
@@ -1,19 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes';
-import imageRoutes from './routes/imageRoutes';
 
+import cors from 'cors';
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/auth', authRoutes);
-app.use('/images', imageRoutes);
+app.use(bodyParser.urlencoded({ extended: true }))
+
+import routerDocs from './routes/docsRoutes'
+import dirRoute from './routes/dirRoutes';
+
+app.use(routerDocs)
+app.use(dirRoute)
 
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the API');
+    res.send("<h1>API Core more info in <a href='/api-docs'>/api-docs</a></h1>");
   });
 
 export default app;
