89da9b8167a87c3806d1d9e893ef6c00d17ff20d
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-23 02:24:57 +0900

    Create docsRoutes.ts

---
+++ Core/src/routes/docsRoutes.ts
@@ -0,0 +1,34 @@
+
+import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from 'swagger-jsdoc';
+
+import PORTProjects from '../Config';
+
+const routerDocs = Router();
+
+const options = {
+    definition: {
+      openapi: "3.1.0",
+      info: {
+        title: "EvaOS",
+        version: "0.0.1",
+        description:
+          "Core for EvaOS",
+      },
+      servers: [
+        {
+          url: `http://localhost:${PORTProjects}`,
+        },
+      ],
+    },
+    apis: ["../controllers/*.ts"],
+  };
+
+  const specs = swaggerDocument(options)
+
+routerDocs.use('/api-docs', swaggerUi.serve);
+routerDocs.get('/api-docs', swaggerUi.setup(specs));
+
+
+export default routerDocs;
