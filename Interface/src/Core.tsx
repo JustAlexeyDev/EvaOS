9ae85f627b95a53ecab5b4ccb14e128735fa53a3
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-26 00:15:38 +0900

    Update Core.tsx

--- Interface/src/Core.tsx
+++ Interface/src/Core.tsx
@@ -4,14 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./Screens/Login/Login";
 import Bsod from "./Api/Libs/VioletClientKernel/Core/Managers/Errors/Bsod";
 import Desktop from "./Screens/Desktop";
-import SSSMprocessSetupSystemManager from "./Api/Libs/VioletClientKernel/Core/SSSM/SSSMprocessSetupSystemManager";
+import SystemSoftwareSetupManager from "./Api/Libs/VioletClientKernel/Core/SSSM/SystemSoftwareSetupManager";
 import LoginChecker from "./Api/Libs/VioletClientKernel/Core/Scripts/Security/LoginChecker";
 
 const Core: React.FC = () => {
     return (
         <div>
             <Routes>
-                <Route path="/" element={<SSSMprocessSetupSystemManager />} />
+                <Route path="/" element={<SystemSoftwareSetupManager />} />
                 <Route path="/Login" element={<Login />} />
                 <Route path="/*" element={<Bsod />} />
                 <Route path="/Desktop" element={<Desktop />} />
