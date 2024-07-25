c58dbe802056e4560596f4d5f40e282667d5bd80
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-26 02:01:41 +0900

    Update CheckMonitorWidth.tsx

--- Interface/src/Api/Libs/VioletClientKernel/Core/Managers/Debug/CheckMonitorWidth.tsx
+++ Interface/src/Api/Libs/VioletClientKernel/Core/Managers/Debug/CheckMonitorWidth.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Check, X } from "lucide-react";
 import "./Debug.css"
+import { useNavigate } from 'react-router';
 
 const CheckMonitorWidth: React.FC = () => {
+    const naviagte = useNavigate();
     const [icon, setIcon] = useState<React.ReactNode>(<X color="red" />);
     const [result, setResult] = useState<string>("Loading...");
 
@@ -12,6 +14,7 @@ const CheckMonitorWidth: React.FC = () => {
             if (width < 600) { 
                 setIcon(<X color="red" />);
                 setResult("Monitor Width fail");
+                naviagte("/MonitorWidthNotSupported");
             } else {
                 setIcon(<Check color="#1eff00" />);
                 setResult("Monitor width pass");
