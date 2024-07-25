cb17f6b89bea84febb7d166cc522f2e695e9f2c5
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-26 01:23:41 +0900

    Create VioletUiAccentButton.jsx

---
+++ Interface/src/Api/Libs/VioletUiLib/Libs/uiElements/Buttions/VioletUiAccentButton.jsx
@@ -0,0 +1,10 @@
+import './Styles/Buttons.css';
+
+const VioletUiAccentButton = ({title}) => {
+    return(
+        <button className="Accent--Button--VioletUi">
+            {title}
+        </button>
+    );
+}
+export default VioletUiAccentButton;
