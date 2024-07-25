d30b7d9dd0e16165838f89efd72011a799271d5b
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-26 00:53:32 +0900

    Update Core.tsx

--- Interface/src/Core.tsx
+++ Interface/src/Core.tsx
@@ -4,7 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./Screens/Login/Login";
 import Bsod from "./Api/Libs/VioletClientKernel/Core/Managers/Errors/Bsod";
 import Desktop from "./Screens/Desktop";
-import SystemSoftwareSetupManager from "./Api/Libs/VioletClientKernel/Core/SSSM/SystemSoftwareSetupManager";
+import SystemSoftwareSetupManager from "./Api/Libs/VioletClientKernel/Core/SystemSoftwareSetupManager/SystemSoftwareSetupManager";
 import LoginChecker from "./Api/Libs/VioletClientKernel/Core/Scripts/Security/LoginChecker";
 
 const Core: React.FC = () => {
