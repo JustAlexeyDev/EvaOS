76ebd19aefa529ed02639a68a53b90fad3c2c98a
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-07-26 05:01:44 +0900

    Update VioletUiAccentButton.jsx

--- Interface/src/Api/Libs/VioletUiLib/Libs/uiElements/Buttions/VioletUiAccentButton.jsx
+++ Interface/src/Api/Libs/VioletUiLib/Libs/uiElements/Buttions/VioletUiAccentButton.jsx
@@ -1,8 +1,8 @@
 import './Styles/Buttons.css';
 
-const VioletUiAccentButton = ({title}) => {
+const VioletUiAccentButton = ({title, onClick}) => {
     return(
-        <button className="Accent--Button--VioletUi">
+        <button className="Accent--Button--VioletUi" onClick={onClick}>
             {title}
         </button>
     );
