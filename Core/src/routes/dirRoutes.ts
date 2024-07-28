156751c1e7695576984d175719560987775afac1
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2024-07-28 16:01:09 +0900

    Bug fix

--- Core/src/routes/dirRoutes.ts
+++ Core/src/routes/dirRoutes.ts
@@ -1,21 +1,26 @@
 import { Router } from 'express';
-import express from 'express';
 import {cat, exportFiles, importFiles, ls, mkdir, remove, ScanApp, touch, tree, versionCoreDir } from '../controllers/dirController';
 import multer from 'multer';
-import path from 'path';
+import rateLimit from 'express-rate-limit';
 const upload = multer();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 минут
+    max: 100, // лимит на 100 запросов с одного IP за 15 минут
+    message: 'Слишком много запросов с этого IP, попробуйте позже.' 
+  });
 
 
 const dirRoute = Router();
                                               // Post:
-dirRoute.post('/mkdir', upload.none(), mkdir) // title, path
-dirRoute.post('/ls', upload.none(), ls) // path
-dirRoute.post('/tree', upload.none(), tree) // path
-dirRoute.post('/remove', upload.none(), remove) // title, path
-dirRoute.post('/import', upload.single('file'), importFiles) // path, file
-dirRoute.post('/export', upload.none(), exportFiles) // path
-dirRoute.post('/touch', upload.none(), touch) // path, title, desc
-dirRoute.post('/cat', upload.none(), cat) // path, title
+dirRoute.post('/mkdir', limiter , upload.none(), mkdir) // title, path
+dirRoute.post('/ls', limiter , upload.none(), ls) // path
+dirRoute.post('/tree', limiter , upload.none(), tree) // path
+dirRoute.post('/remove', limiter , upload.none(), remove) // title, path
+dirRoute.post('/import', limiter , upload.single('file'), importFiles) // path, file
+dirRoute.post('/export', limiter , upload.none(), exportFiles) // path
+dirRoute.post('/touch', limiter , upload.none(), touch) // path, title, desc
+dirRoute.post('/cat', limiter , upload.none(), cat) // path, title
                                         // Get:
 dirRoute.get('/versionCoreDir', upload.none(), versionCoreDir) 
 dirRoute.get('/appScaner', ScanApp)
